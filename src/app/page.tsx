bc4d2b802e8273d2f7439b4d738648020f83d2f3
martin1906s <martin.simbana007@gamil.com>
2025-05-05 17:48:55 -0500

    Creacion de etiquetas meta e implementacion del logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,10 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <Image 
-        src="/file.svg"
+        src="/logo.png"
         alt="Logo mesa del chef"
-        width={180}
-        height={37}
+        width={200}
+        height={200}
         priority
       />
       <h1 className={styles.title}>
